4640dadeb1974fa0830ff60f6b0878419f7ccf8c
Admin <Admin@DESKTOP-G2QUO26>
2021-08-03 02:09:25 +0300

    Init project

---
+++ src/test/java/com/shop/zephyr/ZephyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.zephyr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
